6e3448ce893d60ed0211dca574d9357d4dd583e5
q45 <quintinsmith@icloud.com>
2013-07-16 22:00:54 -0600

    added style and header

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Law::Application.config.secret_key_base = 'bbc661bd2202753ebf5d2a32f883bdbdb1ecc7891f560e939888047ac0abb626ade5179befc4f493fa9b1e5292ba6433dbca04ef375f464f41d2683e107ff55f'
